7885b45cf9bfef5bde7a55d5b4a5ed1f68bcefc3
Jonathan Milgrom <jmilgrom@triplelift.co>
2016-10-19 18:20:49 -0400

    clean up

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import * as types from '../constants/ActionTypes'
 
 export const addMemories = memories => ({type: types.ADD_MEMORIES, memories: memories})
-export const addMemory = text => ({type: types.ADD_MEMORY, memory: {name: text}})
+export const addMemory = name => ({type: types.ADD_MEMORY, memory: {name: name}})
 export const removeMemory = id => ({type: types.REMOVE_MEMORY, id: id})
