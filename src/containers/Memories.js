daa821486fbe387f434077d21cf0f1e1f68d72f4
Jonathan Milgrom <jmilgrom@triplelift.co>
2016-10-20 12:44:45 -0400

    hotness

--- src/containers/Memories.js
+++ src/containers/Memories.js
@@ -1,30 +1,44 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import Memories from '../components/Memories'
 import * as actions from '../actions'
 
-const MemoriesContainer = React.createClass({
-  componentDidMount: function() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() => {
-      this.forceUpdate();
-    });
-  },
-  componentWillUnmount: function() {
-    this.unsubscribe();
-  },
-  render: function() {
-    const { store } = this.context;
-    const memories = store.getState();
-    const boundActionCreators = bindActionCreators(actions, store.dispatch);
-    return (
-      <Memories {...boundActionCreators} memories={memories}/>
-    )
-  }
-})
+/**
+ * What connect()() does internally:
+ *
+ * const MemoriesConnected = React.createClass({
+ *   componentDidMount: function() {
+ *     const { store } = this.context;
+ *     this.unsubscribe = store.subscribe(() => {
+ *       this.forceUpdate();
+ *     });
+ *   },
+ *   componentWillUnmount: function() {
+ *     this.unsubscribe();
+ *   },
+ *   render: function() {
+ *     const { store } = this.context;
+ *     const state = store.getState();
+ *     const boundActionCreators = bindActionCreators(actions, store.dispatch);
+ *     return (
+ *       <Memories {...boundActionCreators} memories={state}/>
+ *     )
+ *   }
+ * })
+ *
+ */
 
-MemoriesContainer.contextTypes = {
+const mapStateToProps = (state) => ({memories: state});
+const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch);
+
+const MemoriesConnected = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Memories);
+
+MemoriesConnected.contextTypes = {
   store: React.PropTypes.object
 };
 
-module.exports = MemoriesContainer;
+module.exports = MemoriesConnected;
